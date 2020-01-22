849b7c57d1399c804de1aa064bbe6bc1713cb964
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-01-22 15:23:53 +0100

    commit-detail component

--- src/components/commit-details/commit-details.js
+++ src/components/commit-details/commit-details.js
@@ -2,15 +2,25 @@ import React, { useEffect } from 'react'
 
 import { connect } from 'react-redux'
 import { selectCommit } from '../../redux/actions';
+import { getSelectedCommitState } from '../../redux/selectors';
 
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import FolderIcon from '@material-ui/icons/Folder';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import Icon from '../icon/icon';
 
 import history from '../../history';
+
+import Moment from 'react-moment';
+
+import './commit-details.css';
 
 function CommitDetails(props) {
 
@@ -18,51 +28,57 @@ function CommitDetails(props) {
         const user = history.location.pathname.split('/')[2];
         const repo = history.location.pathname.split('/')[3];
         const id = history.location.pathname.split('/')[4];
-        props.onRender(user, repo, id);
-    },[]);
+        props.onMount(user, repo, id);
+    }, []);
 
-    return (
-        <Container maxWidth="xs">
+    return props.data ? (
+        <Container maxWidth="xs" className="commit-detail">
             <Grid container={true} direction="column">
-                <Grid item={true}>
-                    <Typography variant="h4">
-                        Commit message
+                <Grid item={true} className="row">
+                    <Typography variant="h4" >
+                        {props.data.commit.message}
                     </Typography>
                 </Grid>
-                <Grid item={true}>
-                    <Grid container={true} direction="row" alignItems="center" spacing={2}>
-                        <Grid item={true}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                <Grid item={true} className="row">
+                    <Grid container={true} direction="row" justify="space-between" alignItems="center" spacing={2}>
+                        <Grid className="author" item={true}>
+                            <Grid container={true} alignItems="center" spacing={2}>
+                                <Grid item={true}>
+                                    <Avatar alt="author avatar" src="/static/images/avatar/1.jpg" />
+                                </Grid>
+                                <Grid item={true}>
+                                    <Typography variant="h6">
+                                        {props.data.commit.author.name}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
                         </Grid>
-                        <Grid item={true}>
-                            <Typography variant="h5">
-                                Commit author
-                            </Typography>
-                        </Grid>
-                        <Grid item={true}>
-                            <Typography variant="h5">
-                                on 35/10/2019 12:00
+                        <Grid className="date" item={true}>
+                            <Typography variant="h6">on&nbsp;
+                                <Moment>
+                                    {props.data.commit.author.date}
+                                </Moment>
                             </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
 
-                <Grid item={true}>
+                <Grid item={true} className="row detail">
                     <Grid container={true} direction="row" alignItems="flex-end" spacing={2} justify="space-between">
-                        <Grid item={true}>
+                        <Grid item={true} className="hash">
                             <Grid container={true} direction="row" alignItems="flex-end" spacing={2}>
                                 <Grid item={true}>
                                     <Icon icon="git-commit"></Icon>
                                 </Grid>
                                 <Grid item={true}>
                                     <Typography variant="h6" >
-                                        adhdgu3rbx3rkjgh4ohlvigh4
-                            </Typography>
+                                        {props.data.sha}
+                                    </Typography>
                                 </Grid>
                             </Grid>
                         </Grid>
 
-                        <Grid item={true}>
+                        <Grid item={true} className="changes">
                             <Grid container={true} spacing={2}>
                                 <Grid item={true}>
                                     <Grid container={true} alignItems="center">
@@ -82,19 +98,48 @@ function CommitDetails(props) {
                                 </Grid>
                             </Grid>
                         </Grid>
+                        <Grid item xs={12} md={6} item={true} className="file-list">
+                            <Typography variant="h6" >
+                                File changed:&nbsp;{props.data.files.length}
+                            </Typography>
+                            <div className="file-list-container" maxWidth="xs">
+                                <List dense={true}>
+                                    {props.data.files.map(f => {
+                                        return <ListItem>
+                                            <ListItemIcon>
+                                                <FolderIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary={f.filename}
+                                                secondary={f.status}
+                                            />
+                                            <div className="file-change">
+                                                <Icon icon="file-plus"></Icon>
+                                                {f.additions}
+                                            </div>
+                                            <div className="file-change">
+                                                <Icon icon="file-minus"></Icon>
+                                                {f.deletions}
+                                            </div>
+                                        </ListItem>
+                                    })
+                                    }
+                                </List>
+                            </div>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
         </Container>
-    )
+    ) : <span />
 }
 
 const mapStateToProps = (state) => ({
-    commit: state.commitReducer.commit
+    data: getSelectedCommitState(state.commitReducer)
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    onRender: (user, repo, id) => dispatch(selectCommit(user, repo, id))
+    onMount: (user, repo, id) => dispatch(selectCommit(user, repo, id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(React.memo(CommitDetails))
