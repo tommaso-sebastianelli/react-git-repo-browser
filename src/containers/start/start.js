271d7d335c426a82855d27b9be172a54af79fccc
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-01-22 11:41:17 +0100

    added appbar to start page

--- src/containers/start/start.js
+++ src/containers/start/start.js
@@ -1,16 +1,34 @@
 import React, { PureComponent } from 'react'
 import './start.css'
 import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+// import IconButton from '@material-ui/core/IconButton';
+// import InfoIcon from '@material-ui/icons/Info';
+
 import Search from './search/search';
 
 class Start extends PureComponent {
 
     render() {
         return (
-            <Container maxWidth="sm" className="container m-t-xl">
-                {/* <AsyncComponent component={<Search/>}></AsyncComponent> */}
-                <Search/>
-            </Container>
+            <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" className="app-bar-title">
+                            React Git Repo Browser
+                        </Typography>
+
+                        {/* <IconButton aria-label="delete">
+                            <InfoIcon/>
+                        </IconButton> */}
+                    </Toolbar>
+                </AppBar>
+                <Container maxWidth="sm" className="container m-t-xl">
+                    <Search />
+                </Container>
+            </div>
         )
     }
 }
