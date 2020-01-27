e9982c1191aaac37e56bbc2affa50fa92d13da65
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-01-27 16:21:55 +0100

    fixed commit data null

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -101,7 +101,7 @@ const selectCommitStart = id => ({
 const selectCommitSuccess = data => ({
   type: SELECT_COMMIT_SUCCESS,
   payload: {
-    commit: null,
+    commit: data,
     loading: false
   }
 });
