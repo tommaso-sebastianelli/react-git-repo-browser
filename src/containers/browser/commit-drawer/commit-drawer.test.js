f61739c7e26c7d7d160c09c28b788a2f61a484c6
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-01-27 10:57:35 +0100

    commit drawer unit-test

--- src/containers/browser/commit-drawer/commit-drawer.test.js
+++ src/containers/browser/commit-drawer/commit-drawer.test.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 
-import { render } from '@testing-library/react'
+import { render, fireEvent, waitForElement, waitForDomChange } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import CommitDrawer from './commit-drawer'
 import { listCommits } from '../../../api.mock';
 import thunk from 'redux-thunk';
+import { MemoryRouter } from 'react-router';
+
 
 import configureStore from 'redux-mock-store';
 const mockStore = configureStore([thunk]);
 
 const getRenderedComponent = async () => {
-    const commitsData = await listCommits().toPromise();
+    const res = await listCommits().toPromise();
     const store = mockStore({
         searchReducer: {
-            commits: Array.from(commitsData),
+            commits: res.data,
             user: 'dummyUser',
             repo: 'dummyRepo'
         },
         commitReducer: {
-            selectedCommitId: '6dcb09b5b57875f334f61aebed695e2e4193db8a'
+            selectedCommitId: null
         },
     });
 
@@ -28,7 +30,7 @@ const getRenderedComponent = async () => {
             <CommitDrawer />
         </Provider>
     )
-    return component;
+    return await waitForElement(() => component);
 }
 
 
@@ -36,4 +38,17 @@ test('render component', async () => {
     const c = await getRenderedComponent();
 
     expect(c.container).toBeInTheDocument();
+})
+
+test('ui shows selected commit', async () => {
+    const c = await getRenderedComponent();
+
+
+    const element = c.getByText('Commit for testing selection');
+    expect(element.parentElement.parentElement.classList).not.toContain('selected');
+    fireEvent.click(element);
+    waitForDomChange().then(() => {
+        // selected grey item
+        expect(element.parentElement.parentElement.classList).toContain('selected');
+    });
 })
