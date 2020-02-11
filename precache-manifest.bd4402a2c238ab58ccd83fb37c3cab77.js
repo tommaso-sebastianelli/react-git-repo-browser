e9fa60c7bda750af8326849778a3bf50d0764a68
Tommaso Sebastianelli <tommasosebastianelli@Tommasos-MBP.office.local>
2020-02-11 17:15:37 +0100

    Updates

--- precache-manifest.bd4402a2c238ab58ccd83fb37c3cab77.js
+++ precache-manifest.bd4402a2c238ab58ccd83fb37c3cab77.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef8f2785746f7e9cfb58f101f01f34a5",
+    "revision": "6a82a489d209669198d4eb25585dd996",
     "url": "/react-git-repo-browser/index.html"
   },
   {
-    "revision": "48bf0c29556939603bc1",
+    "revision": "cedaa9cbba49ff91996b",
     "url": "/react-git-repo-browser/static/css/main.0aa728c5.chunk.css"
   },
   {
-    "revision": "4857cc88adf8025945bb",
-    "url": "/react-git-repo-browser/static/js/2.257eaca7.chunk.js"
+    "revision": "bd4dda0ac816b5b51c09",
+    "url": "/react-git-repo-browser/static/js/2.0f1dc674.chunk.js"
   },
   {
     "revision": "72d6b23e86a78d4acb7acd56f32d8fe7",
-    "url": "/react-git-repo-browser/static/js/2.257eaca7.chunk.js.LICENSE"
+    "url": "/react-git-repo-browser/static/js/2.0f1dc674.chunk.js.LICENSE"
   },
   {
-    "revision": "48bf0c29556939603bc1",
-    "url": "/react-git-repo-browser/static/js/main.a08cfa6f.chunk.js"
+    "revision": "cedaa9cbba49ff91996b",
+    "url": "/react-git-repo-browser/static/js/main.2c9de7be.chunk.js"
   },
   {
     "revision": "c21452e693cb3264f9b7",
